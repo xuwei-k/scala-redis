1c5455511d38a021496cb3de9fe897b4f5dad18a
Michał Siatkowski <michal.siatkowski@cortb.pl>
2019-09-02 19:15:43 +0200

    added cluster docs

--- src/main/scala/com/redis/cluster/KeyTag.scala
+++ src/main/scala/com/redis/cluster/KeyTag.scala
@@ -1,31 +1,28 @@
 package com.redis.cluster
 
 /**
+ * <p>
  * Consistent hashing distributes keys across multiple servers. But there are situations
  * like <i>sorting</i> or computing <i>set intersections</i> or operations like <tt>rpoplpush</tt>
  * in redis that require all keys to be collocated on the same server.
- * <p/>
- * One of the techniques that redis encourages for such forced key locality is called
- * <i>key tagging</i>. See <http://code.google.com/p/redis/wiki/FAQ> for reference.
- * <p/>
- * The trait <tt>KeyTag</tt> defines a method <tt>tag</tt> that takes a key and returns
- * the part of the key on which we hash to determine the server on which it will be located.
- * If it returns <tt>None</tt> then we hash on the whole key, otherwise we hash only on the
- * returned part.
- * <p/>
- * redis-rb implements a regex based trick to achieve key-tagging. Here is the technique
- * explained in redis FAQ:
- * <i>
- * A key tag is a special pattern inside a key that, if preset, is the only part of the key
- * hashed in order to select the server for this key. For example in order to hash the key
- * "foo" I simply perform the CRC32 checksum of the whole string, but if this key has a
- * pattern in the form of the characters {...} I only hash this substring. So for example
- * for the key "foo{bared}" the key hashing code will simply perform the CRC32 of "bared".
- * This way using key tags you can ensure that related keys will be stored on the same Redis
- * instance just using the same key tag for all this keys. Redis-rb already implements key tags.
- * </i>
+ * </p>
+ * <p>
+ * One of the techniques that redis encourages for such forced key locality is called <i>key tagging</i>.
+ * See <a href="https://redis.io/topics/cluster-tutorial#redis-cluster-data-sharding">Redis Cluster data sharding</a>
+ * for reference.
+ * </p>
+ * <p><i>
+ * (...) but the gist is that if there is a substring between {} brackets in a key, only what is inside the string
+ * is hashed, so for example this{foo}key and another{foo}key are guaranteed to be in the same hash slot,
+ * and can be used together in a command with multiple keys as arguments.
+ * </i></p>
  */
 trait KeyTag {
+
+  /**
+   * Takes a key and returns the part of the key on which we hash to determine the server on which it will be located.
+   * If it returns <tt>None</tt> then we hash on the whole key, otherwise we hash only on the returned part.
+   */
   def tag(key: Seq[Byte]): Option[Seq[Byte]]
 }
 
@@ -46,7 +43,7 @@ object KeyTag {
   }
 
   object NoOpKeyTag extends KeyTag {
-    def tag(key: Seq[Byte]) = Some(key)
+    def tag(key: Seq[Byte]): Option[Seq[Byte]] = None
   }
 
 }
